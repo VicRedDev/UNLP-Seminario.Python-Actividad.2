fd3af67007815fffd036e713f69d1269af4f04a2
Vicente Redruello Rossi <vicenteredruello@gmail.com>
2026-04-05 20:22:31 -0300

    Feat(Rounds, Scores): Recorrer rondas, obtener las puntuaciones totales de cada persona de esa ronda, y ordenarlas de mayor a menor

--- src/competition_simulator.py
+++ src/competition_simulator.py
@@ -1,13 +1,21 @@
-def simulateCompetition(
-    rounds: list[
-        dict[
-            "theme":str,
-            "score":dict[
-                str:dict[
-                    str:int
-                ]
-            ]
-        ]
-    ]
-):
-    ...
+def getSortedRoundTotalScores(round: dict[str: dict[str:int]]) -> list[tuple[str, int]]:
+    round_total_scores = []
+    for name, scores in round.items():
+        round_total_scores.append((name, sum(list(scores.values()))))
+
+    return sorted(
+        round_total_scores,
+        key = lambda person_score: person_score[1],
+        reverse = True
+    )
+
+def simulateCompetition(rounds: list[dict["theme":str,"score":dict[str:dict[str:int]]]]):
+    results = {}
+    total_rounds = len(rounds)
+
+    for round_number in range(total_rounds):
+        round = rounds[round_number]
+        round_name = round['theme']
+        round_scores = round['scores']
+
+        sorted_round_total_scores = getSortedRoundTotalScores(round_scores)

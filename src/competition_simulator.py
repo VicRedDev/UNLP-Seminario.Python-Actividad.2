3a95d685c0074d98c5c1c1b8f6740ce345bacd48
Vicente Redruello Rossi <vicenteredruello@gmail.com>
2026-04-05 19:45:21 -0300

    Setup(exercise_10, src): Prepare src folder and exercise_10 call on notebook

---
+++ src/competition_simulator.py
@@ -0,0 +1,13 @@
+def simulateCompetition(
+    rounds: list[
+        dict[
+            "theme":str,
+            "score":dict[
+                str:dict[
+                    str:int
+                ]
+            ]
+        ]
+    ]
+):
+    ...
